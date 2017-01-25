e86ef01a54d60804d287895a0b2c188498b0d021
titaneric <chenyihuang001@gmail.com>
2017-01-24 20:26:50 -0800

    Finishing the zoom and move by mouse, existing the zoom problem

---
+++ geometry/map.py
@@ -0,0 +1,64 @@
+import tkinter as tk
+class  World(tk.Frame):
+    def __init__(self, root):
+        tk.Frame.__init__(self, root)
+        self.canvas = tk.Canvas(self, width=300, height=300, background="bisque")
+        self.xsb = tk.Scrollbar(self, orient="horizontal", command=self.canvas.xview)
+        self.ysb = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.ysb.set, xscrollcommand=self.xsb.set)
+        self.canvas.configure(scrollregion=(0,0,1000,1000))
+
+        self.xsb.grid(row=1, column=0, sticky="ew")
+        self.ysb.grid(row=0, column=1, sticky="ns")
+        self.canvas.grid(row=0, column=0, sticky="nsew")
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        # This is what enables using the mouse:
+        '''
+        self.canvas.bind("<ButtonPress-1>", self.scroll_start)
+        self.canvas.bind("<B1-Motion>", self.scroll_move)
+        '''
+        #linux scroll
+        self.canvas.bind("<Button-4>", self.zoomerP)
+        self.canvas.bind("<Button-5>", self.zoomerM)
+        #windows scroll
+        self.canvas.bind("<MouseWheel>",self.zoomer)
+
+        self.scale = 1
+        self.distance = 30
+    '''
+    def scroll_start(self, event):
+        self.canvas.scan_mark(event.x, event.y)
+
+    def scroll_move(self, event):
+        self.canvas.scan_dragto(event.x, event.y, gain=1)
+    '''
+    #windows zoom
+    def zoomer(self,event):
+        if (event.delta > 0):
+            self.canvas.scale("all", event.x, event.y, 1.1, 1.1)
+        elif (event.delta < 0):
+            self.canvas.scale("all", event.x, event.y, 0.9, 0.9)
+        self.canvas.configure(scrollregion = self.canvas.bbox("all"))
+
+    #linux zoom
+    def zoomerP(self,event):
+        self.canvas.scale("all", event.x, event.y, 1.1, 1.1)
+        self.canvas.configure(scrollregion = self.canvas.bbox("all"))
+        self.scale *= 1.1
+        
+
+    def zoomerM(self,event):
+        self.canvas.scale("all", event.x, event.y, 0.9, 0.9)
+        self.canvas.configure(scrollregion = self.canvas.bbox("all"))
+        self.scale *= 0.9
+        
+    def update_coords(self, coords):
+        new_coords = [coords_i * self.scale for coords_i in coords]
+        return new_coords
+
+    
+
+    
+
--- roadModel.py
+++ roadModel.py
@@ -1,36 +1,44 @@
 from tkinter import *
 from geometry.grid import Grid
+from geometry.map import World
+
 def createIntersection(event, line_distance):
-    x, y = determineCoords(event, line_distance)
-    #print(x, y)
-    tmpGrid = gridMatrix[x][y]
-    if tmpGrid.crossStatus is False:
-        cross = canvas.create_rectangle(tmpGrid.x0, tmpGrid.y0, tmpGrid.x1, tmpGrid.y1, fill = "#808080")
-        gridMatrix[x][y].crossStatus = True
+        x, y = determineCoords(event, line_distance)
+        #print(x, y)
+        tmpGrid = gridMatrix[x][y]
+        if tmpGrid.crossStatus is False:
+            world.canvas.create_rectangle(tmpGrid.x0, tmpGrid.y0, tmpGrid.x1, tmpGrid.y1, fill = "#808080")
+            gridMatrix[x][y].crossStatus = True
         
+        else:
+            world.canvas.create_rectangle(tmpGrid.x0, tmpGrid.y0, tmpGrid.x1, tmpGrid.y1, fill = "#808080", dash = (4, 4))
+            firstSelectedCross = tmpGrid
+            firstSelectedStatus = True
+   
 
 def determineCoords(event, line_distance):
-    for x in range(line_distance,canvas_width + line_distance,line_distance):
-        for y in range(line_distance,canvas_height + line_distance,line_distance):
-            if ((x - line_distance) < event.x < x) and ((y - line_distance) < event.y < y):
-                return (x - line_distance)//line_distance, (y - line_distance)//line_distance
+    itemID = world.canvas.find_closest(event.x, event.y)
+    index = itemID[0]
+    x, y = (index // (canvas_height // line_distance), index % (canvas_height // line_distance) - 1)
+    return x, y
 
-def createGrid(canvas, line_distance):
-    # vertical lines at an interval of "line_distance" pixel
-    for x in range(line_distance,canvas_width,line_distance):
-        canvas.create_line(x, 0, x, canvas_height, fill="#000000")
-    # horizontal lines at an interval of "line_distance" pixel
-    for y in range(line_distance,canvas_height,line_distance):
-        canvas.create_line(0, y, canvas_width, y, fill="#000000")
-    
+def createGrid(world, line_distance):
     for x in range(0, canvas_width, line_distance):
         for y in range(0, canvas_height, line_distance):
+            world.canvas.create_rectangle(x, y, x + line_distance, y + line_distance, fill = "bisque", outline = "#FFFFFF")
             tmpGrid = Grid(x, y, line_distance)
             gridMatrix[x // line_distance][y // line_distance] = tmpGrid
 
-def createRoad():
-    return
-
+def createRoad(event, line_distance):
+    print(event.x, event.y)
+    x, y = determineCoords(event, line_distance)
+    #print(x, y)
+    tmpGrid = gridMatrix[x][y]
+    canvas.create_rectangle(tmpGrid.x0, tmpGrid.y0, firstSelectedCross.x1, firstSelectedCross.y1, fill = "#808080")
+'''
+def checkCoords(event):
+    print(event.x, event.y)
+'''
 root = Tk()
 
 menu = Menu(root)
@@ -46,18 +54,21 @@ canvas_height = 300
 canvas_width = 300
 distance = 30
 gridMatrix = [[Grid() for i in range(0, canvas_width, distance)] for j in range(0, canvas_height, distance)]
-
+firstSelectedCross = Grid()
+firstSelectedStatus = False
 screen = Frame(root)
-canvas = Canvas(screen, width = canvas_width, height = canvas_height)
-canvas.bind("<Button-1>", lambda event, line_distance = distance: createIntersection(event, line_distance))
-
-
+world = World(screen)
+#world.canvas.bind("<Button-1>", checkCoords)
+world.canvas.bind("<Double-Button-1>", lambda event, line_distance = distance: createIntersection(event, line_distance))
+#canvas.bind("<B1-Motion>", lambda event, line_distance = distance: createRoad(event, line_distance))
+world.pack(fill = "both", expand = True)
+#canvas.bind("<ButtonRelease>",lambda event, line_distance = distance: createRoad(event, line_distance))
 #crossBtn = Button(toolbar, text = "add", command = lambda: createIntersection(canvas))
 #crossBtn.pack(side = LEFT, padx = 2, pady = 2)
 #toolbar.pack(side = TOP, fill = X)
 
-canvas.pack()
-createGrid(canvas, distance)
+#canvas.pack()
+createGrid(world, distance)
 screen.pack(side = BOTTOM, fill = X)
 
 
