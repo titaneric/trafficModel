9f449a72da6144164871b77a050e9b28f2bfac94
titaneric <chenyihuang001@gmail.com>
2017-01-25 06:10:26 -0800

    Fix the coordination problem

--- roadModel.py
+++ roadModel.py
@@ -4,7 +4,7 @@ from geometry.map import World
 
 def createIntersection(event, line_distance):
         x, y = determineCoords(event, line_distance)
-        #print(x, y)
+        print(x, y)
         tmpGrid = gridMatrix[x][y]
         if tmpGrid.crossStatus is False:
             world.canvas.create_rectangle(tmpGrid.x0, tmpGrid.y0, tmpGrid.x1, tmpGrid.y1, fill = "#808080")
@@ -18,13 +18,19 @@ def createIntersection(event, line_distance):
 
 def determineCoords(event, line_distance):
     itemID = world.canvas.find_closest(event.x, event.y)
+    print("ID is", itemID)
     index = itemID[0]
-    x, y = (index // (canvas_height // line_distance), index % (canvas_height // line_distance) - 1)
-    return x, y
+    #assert 0 < index <= (canvas_width // line_distance) * (canvas_height // line_distance)
+    index -= 1
+    #First picked intersection
+    if 0 < index < (canvas_width // line_distance) * (canvas_height // line_distance):
+        x, y = (index // (canvas_height // line_distance), index % (canvas_height // line_distance))
+        return y, x
+    #Select the intersection to be the part of road
 
 def createGrid(world, line_distance):
-    for x in range(0, canvas_width, line_distance):
-        for y in range(0, canvas_height, line_distance):
+    for y in range(0, canvas_height, line_distance):
+        for x in range(0, canvas_width, line_distance):
             world.canvas.create_rectangle(x, y, x + line_distance, y + line_distance, fill = "bisque", outline = "#FFFFFF")
             tmpGrid = Grid(x, y, line_distance)
             gridMatrix[x // line_distance][y // line_distance] = tmpGrid
@@ -35,6 +41,14 @@ def createRoad(event, line_distance):
     #print(x, y)
     tmpGrid = gridMatrix[x][y]
     canvas.create_rectangle(tmpGrid.x0, tmpGrid.y0, firstSelectedCross.x1, firstSelectedCross.y1, fill = "#808080")
+'''
+def pickIntersection(event, line_distance):
+    #print(event.x, event.y)
+    x, y = determineCoords(event, line_distance)
+    #print(x, y)
+    tmpGrid = gridMatrix[x][y]
+    world.canvas.create_rectangle(tmpGrid.x0, tmpGrid.y0, tmpGrid.x1, tmpGrid.y1, fill = "#A69E24")
+'''
 '''
 def checkCoords(event):
     print(event.x, event.y)
@@ -60,7 +74,7 @@ screen = Frame(root)
 world = World(screen)
 #world.canvas.bind("<Button-1>", checkCoords)
 world.canvas.bind("<Double-Button-1>", lambda event, line_distance = distance: createIntersection(event, line_distance))
-#canvas.bind("<B1-Motion>", lambda event, line_distance = distance: createRoad(event, line_distance))
+#world.canvas.bind("<Enter>", lambda event, line_distance = distance: pickIntersection(event, line_distance))
 world.pack(fill = "both", expand = True)
 #canvas.bind("<ButtonRelease>",lambda event, line_distance = distance: createRoad(event, line_distance))
 #crossBtn = Button(toolbar, text = "add", command = lambda: createIntersection(canvas))
