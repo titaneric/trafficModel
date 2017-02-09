5c0ea3ad70f7fd8efcc3feb17e160c9419c6a8ad
titaneric <chenyihuang001@gmail.com>
2017-02-09 15:52:14 +0800

    Fix the road builder problem

--- geometry/operation.py
+++ geometry/operation.py
@@ -5,7 +5,9 @@ from model.road import Road
 from model.car import Car
 from geometry.rect import Rect
 from geometry.point import Point
-class  Operation(tk.Frame):
+
+
+class Operation(tk.Frame):
     def __init__(self, root, canvas_height, canvas_width, distance, world):
         tk.Frame.__init__(self, root)
         self.root = root
@@ -13,7 +15,7 @@ class  Operation(tk.Frame):
         self.xsb = tk.Scrollbar(self, orient="horizontal", command=self.canvas.xview)
         self.ysb = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
         self.canvas.configure(yscrollcommand=self.ysb.set, xscrollcommand=self.xsb.set)
-        self.canvas.configure(scrollregion=(0,0,1000,1000))
+        self.canvas.configure(scrollregion=(0, 0, 1000, 1000))
 
         self.xsb.grid(row=1, column=0, sticky="ew")
         self.ysb.grid(row=0, column=1, sticky="ns")
@@ -26,12 +28,12 @@ class  Operation(tk.Frame):
         self.canvas.bind("<ButtonPress-1>", self.scroll_start)
         self.canvas.bind("<B1-Motion>", self.scroll_move)
         self.canvas.bind("<ButtonRelease>", self.ready2CreateRoad)
-        #linux scroll
+        # linux scroll
         self.canvas.bind("<Button-4>", self.zoomerP)
         self.canvas.bind("<Button-5>", self.zoomerM)
-        #windows scroll
-        self.canvas.bind("<MouseWheel>",self.zoomer)
-        self.canvas.bind("<Double-Button-1>", self.drawIntersection)#lambda event, line_distance = distance: self.createIntersection(event, line_distance))
+        # windows scroll
+        self.canvas.bind("<MouseWheel>", self.zoomer)
+        self.canvas.bind("<Double-Button-1>", self.drawIntersection)
         self.buildable = False
         self.movePath = []
         self._running = False
@@ -41,7 +43,6 @@ class  Operation(tk.Frame):
         self.canvas_height = canvas_height
         self.canvas_width = canvas_width
         self.drawGrid()
-        #self.runModel()
         self.drawWorld()
 
     def scroll_start(self, event):
@@ -57,6 +58,7 @@ class  Operation(tk.Frame):
     def scroll_move(self, event):
         if self.buildable is False:
             self.canvas.scan_dragto(event.x, event.y, gain=1)
+            self.update_member()
         else:
             self.movePath.append((self.canvas.canvasx(event.x), self.canvas.canvasy(event.y)))
 
@@ -75,21 +77,32 @@ class  Operation(tk.Frame):
         elif (event.delta < 0):
             self.canvas.scale("all", event.x, event.y, 0.9, 0.9)
         self.canvas.configure(scrollregion = self.canvas.bbox("all"))
+        self.update_member()
 
     #linux zoom
     def zoomerP(self, event):
         self.canvas.scale("all", event.x, event.y, 1.1, 1.1)
         self.canvas.configure(scrollregion = self.canvas.bbox("all"))
         self.scale *= 1.1
+        self.update_member()
 
     def zoomerM(self, event):
         self.canvas.scale("all", event.x, event.y, 0.9, 0.9)
         self.canvas.configure(scrollregion = self.canvas.bbox("all"))
         self.scale *= 0.9
+        self.update_member()
 
-    def update_coords(self, coords):
-        new_coords = [coords_i * self.scale for coords_i in coords]
-        return new_coords
+    def update_member(self):
+        for intersection in self.world.intersections.values():
+            copyIntersection = intersection
+            ID = self.canvas.find_withtag(copyIntersection.id)
+            coords = self.canvas.coords(ID)
+            copyIntersection.rect.x = coords[0]
+            copyIntersection.rect.y = coords[1]
+            copyIntersection.rect.width = coords[2] - coords[0]
+            copyIntersection.rect.height = coords[3] - coords[1]
+            del(self.world.intersections[copyIntersection.id])
+            self.world.intersections[copyIntersection.id] = copyIntersection
 
     def drawIntersection(self, event):
         itemID = self.canvas.find_closest(self.canvas.canvasx(event.x), self.canvas.canvasy(event.y))
